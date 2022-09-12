26ed59af0c2615a574172377597f18339f8cae26
Amir Alimov <amiralimov@Amirs-MacBook-Pro.local>
2022-09-12 10:29:10 +0800

    2012 solved

--- 02157-2012.fsx
+++ 02157-2012.fsx
@@ -4,55 +4,58 @@ type Score = int;;
 type Result = Name * Score;;
 
 // Point 1
-let results = [("Amy", 10); ("Doctor", 15); ("Rory", 1)];;
+let res1:Result = ("Amy", 10);;
+let res2:Result = ("Doctor", 15);;
+let res3:Result = ("Rory", 5);;
+let results:Result list = [res1; res2; res3];;
 
-let rec legalResults = function
+let rec legalResults (results:Result list) =
+  match results with
   | [] -> true
-  | (a,b) :: tail -> (b < 100 && b >= 0) && legalResults tail;;
+  | (a,b) :: tail -> (b <= 100 && b >= 0) && legalResults tail;;
 
 // Point 2
 let max a b = if a < b
-                then b
-                else a;;
+                        then b
+                        else a;;
 
-let rec maxScore = function
+let rec maxScore (results:Result list) =
+  match results with
   | [] -> 0
   | [(a,b)] -> b
   | (a,b) :: tail -> max b (maxScore tail);;
 
 // Point 3
-let best ls =
-  let maximum = maxScore ls
-
-  let rec aux = function
-    | [] -> ("EMPTY", 0)
-    | (a,b) :: tail when b = maximum -> (a,b)
-    | _ :: tail -> aux tail
-  
-  aux ls;;
+let rec best (results:Result list) :Result =
+  match results with
+  | [] -> ("EMPTY", 0)
+  | [(x, y)] -> (x, y)
+  | (x, y) :: ((x1, y1) :: tail) when y < y1 -> best ((x1, y1) :: tail)
+  | (x, y) :: ((x1, y1) :: tail) -> best ((x, y) :: tail);;
 
 // Point 4
-let average ls =
-  let rec aux sum n = function
+let average (results:Result list) =
+  let rec aux sum n (results:Result list) =
+    match results with
     | [] -> sum / n
     | (_, x) :: tail -> aux (sum + float x) (n + 1.0) tail
 
-  aux 0.0 0.0 ls;;
+  aux 0.0 0.0 results;;
 
 // Point 5
-let rec delete (a,b) = function
+let rec delete ((name,score):Result) (results:Result list) =
+  match results with
   | [] -> []
-  | (x,y) :: tail when x = a && y = b -> tail
-  | (x,y) :: tail -> (x,y) :: delete (a,b) tail;;
+  | (x,y) :: tail when x = name && y = score -> tail
+  | (x,y) :: tail -> (x,y) :: delete (name,score) tail;;
 
 // Point 6
-let rec bestN ls n =
-    if List.length ls < n 
+let rec bestN (results:Result list) n =
+    if List.length results < n 
       then failwith "Asking for too many elements"
       else if n <= 0
-        then []
-        else let maxTemp = best ls
-             maxTemp :: bestN (delete maxTemp ls) (n - 1);;
+            then []
+            else best results :: bestN (delete (best results) results) (n - 1);;
   
 // Problem 2 (35%)
 type Typ =
@@ -63,50 +66,143 @@ type Typ =
 type Decl = string * Typ
 
 // Point 1
-let decs = [("2", Integer); ("True", Boolean); ("1", Integer)];;
+let decs:Decl list = [("2", Integer); ("True", Boolean); ("1", Integer)];;
 
-let rec has_one id = function
+let rec distinctVars (declarations:Decl list) = 
+  match declarations with
   | [] -> true
-  | (a,b) :: tail when id = a -> false
-  | _ :: tail -> has_one id tail;;
+  | head :: tail -> not (tail |> List.contains head) && distinctVars tail;;
 
-let rec distinctVars = function
-  | [] -> true
-  | [(_,_)] -> true
-  | (a,_) :: tail -> (has_one a tail) && distinctVars tail;;
-
-
-type SymbolTable = Map<string,Typ>
+type SymbolTable = Map<string, Typ>
   // Point 2
-let toSymbolTable = List.fold (fun (map:SymbolTable) (a,b) -> map.Add(a,b)) Map.empty;;
+let toSymbolTable (declarations:Decl list) = 
+  declarations
+    |> List.fold (fun (map:SymbolTable) (a, b) -> map |> Map.add a b) Map.empty;;
 
 // Point 3
-let extendST sym decls = List.fold (fun (map:SymbolTable) (a,b) -> map.Add(a,b)) sym decls;;
+let extendST (sym:SymbolTable) (declarations:Decl list) =
+  declarations
+    |> List.fold (fun (map:SymbolTable) (a,b) -> map |> Map.add a b) sym;;
 
 type Exp =
   | V of string
   | A of string * Exp list;;
 
 let expression = A (">", [V "x"; V "y"]);;
-// Point 4
-let rec symbolsDefined (sym: SymbolTable) (exp: Exp) =
-  let rec aux (sym: SymbolTable) = function
-    | V x -> not (sym.TryFind(x) = None)
-    | A (x, ls) -> not (sym.TryFind(x) = None) && aux' ls
-  and aux' = function
-    | [] -> true
-    | x :: tail -> aux sym x && aux' tail
+let madd:SymbolTable =
+  Map.empty
+  |> Map.add ">" Boolean
+  |> Map.add "x" Integer
+  |> Map.add "y" Integer;;
 
-  aux sym exp;;
+// Point 4
+let rec symbolsDefined (sym:SymbolTable) (exp:Exp) =
+  match exp with
+  | V x -> not ((sym |> Map.tryFind x) = None)
+  | A (x, xs) -> not ((sym |> Map.tryFind x) = None) && aux sym xs
+
+and aux (sym:SymbolTable) (expLs:Exp list) =
+  match expLs with
+  | [] -> true
+  | head :: tail -> symbolsDefined sym head && aux sym tail;;
 
 // Point 5
-let typOf (sym: SymbolTable) (exp: Exp) =
-  let rec aux (sym: SymbolTable) x =
-    match sym.TryFind(x) with
-      | Some y -> y
-      | None -> failwith "not strongly typed"
-
+let rec typOf (sym:SymbolTable) (exp:Exp) =
   match exp with
-    | V x -> aux sym x
-    | A (x, ls) -> aux sym x;;
-  
+  | V x -> aux' (sym |> Map.tryFind x)
+  | A (x, xs) -> aux' (sym |> Map.tryFind x)
+
+and aux' (typ:Typ option) =
+  match typ with
+  | Some x -> x
+  | None -> failwith "Not well typed";;
+
+type Stm =
+  | Ass of string * Exp
+  | Seq of Stm * Stm
+  | Ite of Exp * Stm * Stm
+  | While of Exp * Stm
+  | Block of Decl list * Stm;;
+
+// Point 6
+let rec wellTyped (sym:SymbolTable) (stm:Stm) =
+  match stm with
+  | Ass (x, e) -> ((sym |> Map.tryFind x) = None) && ((sym |> Map.find x) = (typOf sym e))
+  | Seq (stm1, stm2) -> wellTyped sym stm1 && wellTyped sym stm2
+  | Ite (e, stm1, stm2) -> (typOf sym e = Boolean) && wellTyped sym stm1 && wellTyped sym stm2
+  | While (e, stm) -> (typOf sym e = Boolean) && wellTyped sym stm
+  | Block (decls, stm) -> distinctVars decls && wellTyped (extendST sym decls) stm;;
+
+// Problem 3 (20%)
+let rec h a b =
+  match a with
+  | [] -> b
+  | c :: d -> c :: (h d b);;
+
+type T<'a, 'b> =
+  | A of 'a
+  | B of 'b
+  | C of T<'a, 'b> * T<'a, 'b>;;
+
+let rec f1 (t:T<'a,'b>) =
+  match t with
+  | C (t1,t2) -> 1 + max (f1 t1) (f1 t2)
+  | _ -> 1;;
+
+let rec f2 = function
+  |A e |B e ->[e]
+  |C(t1,t2)->f2 t1 @ f2 t2;;
+
+let rec f3 e b t=
+  match t with
+  | C (t1,t2) when b -> C(f3 e b t1, t2)
+  | C (t1,t2) -> C (t1,f3 e b t2)
+  | _ when b -> C (A e,t)
+  | _ -> C (t, B e);;
+
+// Point 1
+(*
+  h : 'a list -> 'a list -> 'a list
+  it concatenates the two lists a and b
+*)
+
+// Point 2
+let a1 = A 2;;
+let b1 = B true;;
+let c1 = C (a1, b1);;
+
+// Point 3
+let c2 = C (A [2; 3; 4], B (Some true));;
+
+// Point 4
+// f1 : T<'a, 'b> -> int
+// f1 counts the number of nodes in a tree
+
+// f2 :  T<'a, 'a> -> 'a list
+// f2 makes a list of values from the nodes in a tree
+
+// f3 : 'a -> bool -> T <'a, 'a> -> T <'a, 'a>
+// f3 if b then it goes to the left branch otherwise right, and if it encounters a leaf it replaces it by a branch (A e, t) or (t, B e) accordingly
+
+// Problem 4 (20%)
+type 'a tree =
+  | Lf
+  | Br of 'a * 'a tree * 'a tree;;
+
+let rec sumTree = function
+  | Lf -> 0
+  | Br (x, t1, t2) -> x + sumTree t1 + sumTree t2;;
+
+let rec toList = function
+  | Lf -> []
+  | Br (x, t1, t2) -> x :: (ToList t1 @ toList t2);;
+
+let rec sumList = function
+  | [] -> 0
+  | x :: xs -> x + sumList xs;;
+
+let rec sumListA n = function
+  | [] -> n
+  | x :: xs -> sumListA (n + x) xs;;
+
+// TRIVIAL
