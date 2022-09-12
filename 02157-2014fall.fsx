4a5c6a4b1a64f53c605c40b8eeee2937c8cc58b1
Amir Alimov <amiralimov@Amirs-MacBook-Pro.local>
2022-09-12 16:31:38 +0800

    2014fall solved

--- 02157-2014fall.fsx
+++ 02157-2014fall.fsx
@@ -1,51 +1,63 @@
 // Problem 1 (20%)
 type Rel<'a,'b> = ('a * 'b list) list;;
 
-let rel : Rel<int, string> = [(1, ["a"; "b"; "c"]); (4, ["b"; "e"])];;
+let rel:Rel<int, string> = [(1, ["a"; "b"; "c"]); (4, ["b"; "e"])];;
 
 // Point 1
-let rec apply a = function
+let rec apply a (rel:Rel<'a,'b>) =
+  match rel with
   | [] -> []
-  | (x, xs) :: tail -> if x = a
-                        then xs
-                        else apply a tail;;
+  | (x, xs) :: tail when x = a -> xs
+  | _ :: tail -> apply a tail;;
 
 // Point 2
-let rec inRelation x y = function
+let rec inRelation x y (rel:Rel<'a,'b>) =
+  match rel with
   | [] -> false
-  | (xx, xs) :: tail -> if xx = x
-                          then List.contains y xs
-                          else inRelation x y tail;;
+  | (a, b) :: tail when x = a -> b |> List.contains y
+  | _ :: tail -> inRelation x y tail;;
 
 // Point 3
-let rec insert x y = function
+let rec insert x y (rel:Rel<'a,'b>) =
+  match rel with
   | [] -> [(x, [y])]
-  | (xx, xs) :: tail -> if xx = x
-                          then (xx, y :: xs) :: tail
-                          else (xx, xs) :: insert x y tail;;
+  | (a, b) :: tail when a = x -> (a, (y :: b)) :: tail
+  | head :: tail -> head :: insert x y tail;;
 
 // Point 4
-let rec toRel ls =
-  let rec aux rel = function
-    | [] -> rel
-    | (x, y) :: tail -> aux (insert x y rel) tail
+let rec toRel (ls:('a * 'b) list) =
+  let rec aux (ls:('a * 'b) list) (rel:Rel<'a,'b>) =
+    match ls with
+    | [] -> []
+    | (x, y) :: tail -> aux tail (insert x y rel)
 
-  aux [] ls;;
+  aux ls [];;
 
 // Problem 2 (25%)
 // Point 1
-let multTable n = Seq.init 10 (fun x -> (x+1)*n);;
+let multTable n =
+  (+) 1
+  |> Seq.initInfinite 
+  |> Seq.filter (fun x -> x%n = 0)
+  |> Seq.take 10;;
 
 // Point 2
-
+let rec tableOf x y f = seq{
+  for i in [1..x] do
+    for j in [1..y] do
+      yield (i, j, f i j)
+};;
 
 // Point 3
-let rec an n = 
-  if n = 0
-    then ""
-    else "a" + an (n-1);;
+let rec repeatChar x =
+  match x with
+  | 0 -> ""
+  | n -> "a" + repeatChar (n - 1);;
 
-let p3 = Seq.init 10 (fun x -> an (x + 1));;
+let aSeq =
+  (+) 1
+  |> Seq.initInfinite
+  |> Seq.map (fun x -> repeatChar x);;
 
 let rec f i = function
   | [] -> []
@@ -61,21 +73,16 @@ let rec f' acc i = function
   | [] -> List.rev acc
   | x :: xs -> f' ((x + i) :: acc) (i * 1) xs;;
 
-let rec f'' cc acc i ls =
-  if cc = 0
-    then List.rev acc
-    else match ls with 
-          | [] -> failwith "Too many continuation calls"
-          | x :: xs -> f'' (cc - 1) ((x + i) :: acc) (i * i) xs;;
+// Continuation based tail-recursion is not for me :(
 
 // Problem 3 (20%)
-type T<'a> = N of 'a * T<'a> list
+type T<'a> = N of 'a * T<'a> list;;
 
-let rec f (N (e, es)) = e :: g es
+let rec fa (N (e, es)) = e :: g es
 
 and  g = function
       | [] -> []
-      | e :: es -> f e @ g es;;
+      | e :: es -> fa e @ g es;;
 
 let rec h p = function
   | N (e, _) when p e -> N (e, [])
@@ -106,25 +113,69 @@ let t3 = N (3, [t1;t2]);;
 // Problem 4
 type  Outcome =
   | S
-  | F
+  | F;;
 
-type Sample = Outcome list
+type Sample = Outcome list;;
+
 type ProbTree =
   | Branch of string * float * ProbTree * ProbTree
-  | Leaf of string
+  | Leaf of string;;
 
-let exp  = Branch(">2", 0.67, Branch(">3", 0.5, Leaf "A", Leaf "B"), Branch(">3", 0.5, Leaf "C", Leaf "D"))
+let exp  = 
+  Branch(">2", 0.67, 
+    Branch(">3", 0.5, Leaf "A", Leaf "B"), 
+    Branch(">3", 0.5, Leaf "C", Leaf "D"));;
 
 // Point 1
-let rec probOK = function
+let rec probOK (t:ProbTree) =
+  match t with
   | Leaf _ -> true
   | Branch (_, x, b1, b2) -> (0.0 <= x && x <= 1.0) && probOK b1 && probOK b2;;
 
 // Point 2
-let rec isSample = function
+let rec isSample ((os:Sample),(t:ProbTree)) =
+  match (os,t) with
   | ([], Leaf _) -> true
   | ([], Branch _) -> false
   | (x :: xs, Leaf _) -> false
   | (x :: xs, Branch (_, _, tl, tr)) -> match x with
                                           | S -> isSample (xs, tr)
                                           | F -> isSample (xs, tl);;
+
+let ds =([(F, ">2"); (S, ">3")], 0.165, "C");;
+
+// Point 3
+type Description = (Outcome * string) list * float * string;;
+
+let rec descriptionOf (os:Sample) (t:ProbTree) =
+  let rec aux (os:Sample) (t:ProbTree) ((ls, n, str):Description) =
+    match (os,t) with
+    | ([], Leaf x) -> (ls |> List.rev, n, x)
+    | (x :: xs, Branch (s, p, tl, tr)) -> match x with
+                                            | S -> aux xs tl (((S, s) :: ls), p * n, str)
+                                            | F -> aux xs tr (((F, s) :: ls), (1.0 - p) * n, str)
+    | _ -> failwith "Invalid Sample"
+  
+  aux os t ([], 1.0, "");;
+
+// Point 4
+let allDescriptions (t:ProbTree) :Set<Description> =
+  let rec aux ((ls, n, str):Description) (t:ProbTree) =
+    match t with
+    | Leaf x -> [(List.rev ls, n, x)]
+    | Branch (ds, p, tr, tl) -> aux (((S, ds) :: ls), p * n, str) tr @ aux (((F, ds) :: ls), (1.0 - p) * n, str) tl
+
+  aux ([], 1.0, "") t |> Set.ofList;;
+
+// Point 5
+let rec probabilityOf (t:ProbTree) pred =
+  let rec aux (descs:Set<Description>) pred =
+    match (descs |> Set.toList) with
+    | [] -> 0.0
+    | (_, p, c) :: tail when pred c -> p
+    | _ :: tail -> aux (tail |> Set.ofList) pred 
+  
+  aux (allDescriptions t) pred;;
+
+// Point 6
+// TRIVIAL
