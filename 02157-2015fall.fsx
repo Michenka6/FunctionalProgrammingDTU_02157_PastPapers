ec140a68f978f9ce84c0add0a15bb57c0b9f5323
Amir Alimov <amiralimov@Amirs-MacBook-Pro.local>
2022-09-12 20:38:27 +0800

    2015 fall solved

--- 02157-2015fall.fsx
+++ 02157-2015fall.fsx
@@ -1,58 +1,66 @@
-// Problem (30%)
+// Problem 1 (30%)
 type Appliance = string;;
 type Usage = Appliance * int;;
 
-let ad1 = ("washing machine", 2);;
-let ad2 = ("coffee machine", 1);;
-let ad3 = ("dishwasher", 2);;
-let ats = [ad1; ad2; ad3; ad1; ad2];;
+let ad1:Usage = ("washing machine", 2);;
+let ad2:Usage = ("coffee machine", 1);;
+let ad3:Usage = ("dishwasher", 2);;
+let ats:Usage list = [ad1; ad2; ad3; ad1; ad2];;
 
 // Point 1
-let rec inv = function
-	|	[] -> true
-	| (_, x) :: tail -> (x > 0) && inv tail;;
+let rec inv (ls:Usage list) =
+    match ls with
+    |    [] -> true
+    | (_, x) :: tail -> (x > 0) && inv tail;;
 
 // Point 2
-let rec durationOf a = function
-	| [] -> 0
-	| (x, y) :: tail when a = x -> y + durationOf a tail
-	| _ :: tail -> durationOf a tail;;
+let rec durationOf (a:Appliance) (ls:Usage list) =
+    match ls with
+    | [] -> 0
+    | (x, y) :: tail when a = x -> y + durationOf a tail
+    | _ :: tail -> durationOf a tail;;
 
 // Point 3
-let rec wellFormed ls =
-	let rec durationOf = function
-		| [] -> 0
-		| (_, x) :: tail -> x + durationOf tail
+let rec wellFormed (ls:Usage list) =
+    let rec aux (ls:Usage list) =
+        match ls with
+        | [] -> 0
+        | (_, x) :: tail -> x + aux tail
 
-	(inv ls) && (durationOf ls <= 24);;
+    (inv ls) && (aux ls <= 24);;
 
 // Point 4
-let rec delete a = function
-	| [] -> failwith "Can't delete a non-existing element"
-	| (x, y) :: tail when x = a -> tail
-	| x :: tail -> x :: delete a tail;;
+let rec delete (a:Appliance) (ls:Usage list) =
+    match ls with
+    | [] -> failwith "Can't delete a non-existing element"
+    | (x, y) :: tail when x = a -> tail
+    | x :: tail -> x :: delete a tail;;
 
 type Price = int;;
 type Tariff = Map<Appliance, Price>;;
 
 // Point 5
-let rec isDefined (trf:Tariff) = function
-	| [] -> true
-	| (x, _) :: tail -> match trf.TryFind x with
-												| Some y -> isDefined trf tail
-												| None -> false;;
+let rec isDefined (ats:Usage list) (trf:Tariff) =
+    match ats with
+    | [] -> true
+    | (x, _) :: tail -> 
+        match trf.TryFind x with
+        | Some y -> isDefined tail trf
+        | None -> false;;
 
 // Point 6
-let rec priceOf (trf:Tariff) = function
-	| [] -> 0
-	| (x, y) :: tail -> match trf.TryFind x with
-												| Some z -> z * y + priceOf trf tail
-												| None -> failwith "Can't find the item in the Tariff map"
+let rec priceOf (ats:Usage list) (trf:Tariff) =
+    match ats with
+    | [] -> 0
+    | (x, y) :: tail -> 
+        match trf.TryFind x with
+        | Some z -> z * y + priceOf tail trf
+        | None -> failwith "Can't find the item in the Tariff map"
 
 // Problem 2 (35%)
 let rec g1 p = function
-	| x::xs when p x -> x :: g1 p xs
-	|_ -> [];;
+    | x::xs when p x -> x :: g1 p xs
+    |_ -> [];;
 
 let rec g2 f h n x =
   match n with
@@ -62,21 +70,21 @@ let rec g2 f h n x =
 
 // Point 1
 (*
-	g1 : ('a -> bool) -> 'a list -> 'a list
+    g1 : ('a -> bool) -> 'a list -> 'a list
 
-	g2 : ('a -> 'a) -> ('a -> 'a) -> int -> 'a -> 'a
+    g2 : ('a -> 'a) -> ('a -> 'a) -> int -> 'a -> 'a
 *)
 
 // Point 2
 let rec g1' acc p = function
-	| x :: xs when p x -> g1' (x :: acc) p xs
-	| _ -> List.rev acc;;
+    | x :: xs when p x -> g1' (x :: acc) p xs
+    | _ -> List.rev acc;;
 
 // Problem 3 (35%)
 type Name = string;;
 type Flow = int;;
 type River =
-	R of Name * Flow * Tributaries
+    R of Name * Flow * Tributaries
 
 and Tributaries = River list;;
 
@@ -88,52 +96,47 @@ let riv2 = R ("R2", 15, [riv4]);;
 let riv = R ("R", 10, [riv1; riv2; riv3]);;
 
 // Point 2
-let rec contains n = function
-	| R (x, _, []) -> x = n
-	| R (x, y, head :: tail) -> x = n || (contains n head) || (contains n (R (x, y, tail)));;
+let rec contains n (riv:River) =
+    match riv with
+    | R (x, _, []) -> x = n
+    | R (x, y, head :: tail) -> x = n || (contains n head) || (contains n (R (x, y, tail)));;
 
 // Point 3
-let rec allNames = function
-	| R (x, _, []) -> [x]
-	| R (x, y, head :: tail) -> allNames head @ allNames (R (x, y, tail));;
+let rec allNames (riv:River) =
+    match riv with
+    | R (x, _, []) -> [x]
+    | R (x, y, head :: tail) -> allNames head @ allNames (R (x, y, tail));;
 
 // Point 4
-let rec totalFlow = function
-	| R (_, x, []) -> x
-	| R (x, y, head :: tail) -> totalFlow head + totalFlow (R (x, y, tail));;
+let rec totalFlow (riv:River) =
+    match riv with
+    | R (_, x, []) -> x
+    | R (x, y, head :: tail) -> totalFlow head + totalFlow (R (x, y, tail));;
 
 // Point 5
-let rec mainSource riv =
-	let rec aux = function
-		| R (x, y, []) -> [(x, y)]
-		| R (x, y, head :: tail) -> aux head @ aux (R (x, y, tail))
+let rec mainSource (riv:River) =
+    let rec aux (riv:River) =
+        match riv with
+        | R (x, y, []) -> [(x, y)]
+        | R (x, y, head :: tail) -> aux head @ aux (R (x, y, tail))
 
-	let rec aux' = function
-		| [] -> failwith "Empty list"
-		| [(x, y)] -> (x, y)
-		| (x,y) :: ((x1, y1) :: tail) -> if y < y1
-																			then aux' ((x1, y1) :: tail)
-																			else aux' ((x, y) :: tail)
+    let rec aux' (ls:(Name * Flow) list)=
+        match ls with
+        | [] -> failwith "Empty list"
+        | [(x, y)] -> (x, y)
+        | (x,y) :: ((x1, y1) :: tail) -> 
+            if y < y1
+                then aux' ((x1, y1) :: tail)
+                else aux' ((x, y) :: tail)
 
-	aux' (aux riv);;
+    aux' (aux riv);;
 
-let rec tryInsert n t = function
-	| R (x, y, z) when x = n -> Some (R (x, y, z))
-	| R (x, y, []) -> Some (R (x, y, [t]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let rec tryInsert n (t:River) (riv:River) = 
+    match riv with
+    | R (x, y, []) when x = n -> Some (R (x, y, [t]))
+    | R (x, y, head :: tail) when x = n -> Some (R (x, y, t :: (head :: tail)))
+    | R (x, y, head :: tail) -> 
+        if (tryInsert n t head) = None
+            then tryInsert n t (R (x, y, tail))
+            else tryInsert n t head
+    | _ -> None;;
